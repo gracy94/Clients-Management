3413b42ad6bc1d1a6ac387283f31d6cdc6775faf
gracy94 <gracielafaria94@gmail.com>
2023-06-22 11:22:42 -0300

    Link and useLocation added for navigation

--- customer-management/src/components/Layout.jsx
+++ customer-management/src/components/Layout.jsx
@@ -1,11 +1,28 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link, useLocation } from 'react-router-dom'
 
 function Layout() {
+
+  const location = useLocation()
+  console.log(location)
+
   return (
-    <div>
-        <h1 className='text-6xl font-bold'>CRM - React</h1>
-        
-        <Outlet/>
+    <div className='md:flex md:min-h-screen'>
+        <aside className='md:w-1/4 bg-purple-900 px-5 py-10'>
+            <h2 className='text-4xl font-black text-center text-white'>CRM - Clients</h2>
+
+            <nav className='mt-10'>
+                <Link 
+                  className={`${location.pathname === '/' ? 'text-purple-300' : 'text-white'} text-2xl block mt-2 hover:text-purple-300 text-white`} 
+                  to="/">Home</Link>
+                <Link 
+                  className={`${location.pathname === '/clients/new' ? 'text-purple-300' : 'text-white'} text-2xl block mt-2 hover:text-purple-300 text-white`} 
+                  to="/clients/new">New Client</Link>
+            </nav>
+        </aside>
+
+        <main className='md:w-3/4 p-10 md:h-screen overflow-scroll'>
+            <Outlet/>
+        </main>
     </div>
   )
 }
--- customer-management/src/pages/Index.jsx
+++ customer-management/src/pages/Index.jsx
@@ -1,7 +1,7 @@
 
 function Index() {
   return (
-    <div>Home</div>
+    <div>HomePage</div>
   )
 }
 
