ae6d5f69cd0db61062c45300fae646b066380da9
gracy94 <gracielafaria94@gmail.com>
2023-06-26 11:35:21 -0300

    Adding a return button with useNavigate to return last page

--- customer-management/src/pages/NewClient.jsx
+++ customer-management/src/pages/NewClient.jsx
@@ -1,7 +1,23 @@
+import { useNavigate } from "react-router-dom"
 
 function NewClient() {
+  
+  const navigate = useNavigate()
   return (
-    <div>NewClient</div>
+    <>
+      <h1 className="font-black text-4xl text-purple-900">New Client</h1>
+      <p className="mt-3">Complete all the fields to register a new client</p>
+    
+      <div className="flex justify-end">
+        <button className="bg-purple-800 text-white px-3 py-1 font-bold uppercase"
+                onClick={()=> navigate(-1)}
+        >
+          Return
+        </button>
+      </div>
+
+      
+    </>
   )
 }
 
