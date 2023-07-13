65efbe1335426dcbc0a78ed885e86a648ad8e965
gracy94 <gracielafaria94@gmail.com>
2023-07-13 11:48:00 -0300

    errors validation added for email and empty fields

--- customer-management/src/pages/NewClient.jsx
+++ customer-management/src/pages/NewClient.jsx
@@ -1,13 +1,36 @@
-import { useNavigate, Form } from "react-router-dom"
+/* eslint-disable react-refresh/only-export-components */
+import { useNavigate, Form, useActionData } from "react-router-dom"
 import Formu from "../components/Formu"
+import Error from "../components/Error"
 
-export async function action() {
-  console.log('submit on form')
+export async function action({request}) {
+   const formData = await request.formData()
+   const data = Object.fromEntries(formData)
+   const email = formData.get('email')
+
+   //Validation
+   const errors = []
+   if(Object.values(data).includes('')){
+      errors.push('All fields are required')
+   }
+
+   // eslint-disable-next-line no-control-regex
+   let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+   if (!regex.test(email)) {
+    errors.push('email is not valid')
+   }
+
+   if(Object.keys(errors).length) {
+    return errors
+   } 
+   return null
 }
 
 function NewClient() {
   
+  const errors = useActionData()
   const navigate = useNavigate()
+
   return (
     <>
       <h1 className="font-black text-4xl text-purple-900">New Client</h1>
@@ -23,8 +46,11 @@ function NewClient() {
 
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
         
+        {errors ?.length && errors.map((error , i) => <Error key={i}>{error}</Error>)}
+
         <Form
           method="POST"
+          noValidate
         >
             <Formu/>
 
